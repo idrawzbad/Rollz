17e23c64e69be51f323882195fa7d8ec69c2a00c
idrawzbad <39500790+idrawzbad@users.noreply.github.com>
2018-06-06 12:57:55 -0700

    Added Randomized Text for Adventure Mode

--- Dice/js/logic/adventure.js
+++ Dice/js/logic/adventure.js
@@ -17,31 +17,115 @@ adventurebutton.onclick = function () {
 
     //Dice Logic
 
+    //Pull adventure string
+    function getRandomText(obj, item) {
+      // 1) select which property to operate on.
+      var itemArr = obj[item];
+
+      // 2) we already know that the key can only be an integer(between 0 and length -1 )
+
+      // Math.random()   : to generate a random number(between [0-1]).
+      // Math.round()    : to round a number to an integer.
+      // number % length : to make sure the result would be an integer between 0 and length -1 
+      var index = Math.round((Math.random() * itemArr.length)) % itemArr.length;
+
+
+      return {
+        value: itemArr[index],
+      }
+
+    }
+
     //ADV Result 1 - Obtain Item
     if (result > advresultstart1 && result < advresultend1) {
-      message = "ADVR Result 1 - " + result + "<br>";
+      /*  
+      if (confirm("Replace Weapon?")) {
+  
+          slots = 5;
+  
+          // save current item as last item & display it
+          lastitempower = power;
+          lastitemslots = slots;
+          lastitemimage = document.getElementById('itemimage').src;
+          lastitemtitle = document.getElementById('itemtitle').innerHTML;
+          LastItemDisplay();
+  
+          // change opacity of last item
+          document.getElementById('lastitem').style.opacity = "1";
+  
+          // reset and show slot and power
+          power = defaultpower;
+          slots = defaultslots;
+          SlotsDisplay();
+          PowerDisplay();
+  
+          // Update the image
+          document.getElementById('itemimage').src = 'assets/halberd.png';
+          document.getElementById('itemtitle').innerHTML = 'The ShatterMoon';
+  
+          // Hide / Show Mod buttons based on slots left
+          SlotCheck();
+  
+          message = "You've gained a new item! " + "Your last item had " + power + " power. Outcome 1 - Roll:" + result + "<br>";
+          $("#outcome").append(message);
+      
+    } else {
+      message = "You threw away the item. Outcome 1 - Roll:" + result + "<br>";
       $("#outcome").append(message);
+    }
+*/
     }
     //ADV Result 2 - Obtain 100% Adventure Fee
     else if (result > advresultstart2 && result < advresultend2) {
       cash = cash + adventurebuttoncash;
-      message = "ADVR Result 2 - " + result + "<br>";
+
+      var answers = [
+        "You trip into a pile of poop. But inside the poop you find some gold!",
+        "A bandit appears from a tree and threatens you. However, they fall from the tree and die.",
+        "A genie appears from no where and says, 'Try again.' ",
+        "A troll stops your way and says, 'Pay the toll or none shall pass.' You convince the troll it's reverse day.",
+        "You find a box of cereal that has been aged to perfection. Wait a minute... there's a prize!"
+      ]
+
+      var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+      message = randomAnswer + " You have gained " + adventurebuttoncash + " gold. <br>";
       $("#outcome").append(message);
     }
     //ADV Result 3 - Obtain 200% Adventure Fee
     else if (result > advresultstart3 && result < advresultend3) {
       cash = cash + (2 * adventurebuttoncash);
-      message = "ADVR Result 3 - " + result + "<br>";
+      var answers = [
+        "A sultan appear before you and asks you to find his son. You point at the sun and he pays you to go away.",
+        "You find a buried chest. Inside the chest, you find a geocache number and instructions not to take too much.",
+        "A mystical crab jumps off your dinner plate and tells you to find his family. You raid the kitchen. ",
+        "Ten dwarves are looking for trouble. You jump out and say, 'I AM TROUBLE!' They laugh until they cry and tip you.",
+        "You find a wanted poster at a tavern and realize it's you! You promptly turn yourself in for the reward."
+      ]
+
+      var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+      message = randomAnswer + " You have gained " + (2 * adventurebuttoncash) + " gold. <br>";
       $("#outcome").append(message);
     }
     //ADV Result 4 - Obtain 1000% Adventure Fee
-    else if (result > advresultstart3 && result < advresultend3) {
+    else if (result > advresultstart4 && result < advresultend4) {
       cash = cash + (10 * adventurebuttoncash);
-      message = "ADVR Result 4 - " + result + "<br>";
+      var answers = [
+        "You discover an oasis in the middle of the desert with a crashed alien ship made of gold parts. You scavenge. ",
+        "As you walk through a jungle, you fall into a pit that slides into an abandoned dragon's lair. They left behind some gold. ",
+        "An old beggar asks for change. You give them a gold coin. They suddenly morph into a demon and cry uncontrollably.",
+        "While walking down a path, an ominous cloud appears overhead and begins raining... GOLD!",
+        "A stranger chases after you down the street. 'Mister! You dropped your wallet!' You say nothing."
+      ]
+
+      var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+      message = randomAnswer + " You have gained " + (10 * adventurebuttoncash) + " gold. <br>";
       $("#outcome").append(message);
     }
     //ADV Result 5 - Obtain 50% Adventure Fee
-    else if (result > advresultstart3 && result < advresultend3) {
+    else if (result > advresultstart5 && result < advresultend5) {
       cash = cash + (0.5 * adventurebuttoncash);
       message = "ADVR Result 5 - " + result + "<br>";
       $("#outcome").append(message);
--- Dice/js/values/values.js
+++ Dice/js/values/values.js
@@ -1,5 +1,10 @@
-var cash = 2000;
+var cash = 20000;
 var power = 100;
+var slots = 5;
+
+// setting default values for reset
+var defaultpower = 100;
+var defaultslots = 5;
 
 //Buttons
 var button1 = document.getElementById('button1');//60%
@@ -21,26 +26,34 @@ var dicepower3 = 80;
 var diceprob1 = 40; //60%
 var diceprob2 = 10; //90%
 var diceprob3 = 90; //10%
-var slots = 5;
 
-var advresultstart1 = 1; //Obtain Item (30%)
-var advresultstart2 = 301; //Obtain 100% of your Adventure fee (20%)
+//Adventure Odds
+
+var advresultstart1 = 1; //Obtain Item (20%)
+var advresultstart2 = 201; //Obtain 100% of your Adventure fee (30%)
 var advresultstart3 = 501; //Obtain 200% of your Adventure fee (9%)
 var advresultstart4 = 591; //Obtain 1000% of your Adventure fee (1%)
 var advresultstart5 = 601; //Obtain 50% of your Adventure fee (30%)
 
-var advresultend1 = 300;
+var advresultend1 = 200;
 var advresultend2 = 500;
 var advresultend3 = 590;
 var advresultend4 = 600;
 var advresultend5 = 900;
 
+//Adventure Dialogue Strings
 
+var advresultstring_old = {
+    string1:"Potato 1 has been located.",
+    string2:"Potato 2 has been located.",
+    string3:"Potato 3 has been located."
+}
 
-// setting default values for reset
-var defaultpower = 100;
-var defaultslots = 5;
-
+var advresults = {
+    advresultstring1: ["star wars", "lotr", "moonlight", "avengers"],
+    advresultstring2: ["scott pilgrim", "harry potter", "eragon"],
+    advresultstring3: ["starbound", "skyrim", "bioshock", "fallout"]
+}
 
 //Begin
 document.getElementById('cash').innerHTML = 'Cash : $' + cash;
